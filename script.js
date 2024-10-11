90ca3e95a76b6e60d7ba314bf54c41ca7760cc42
Ann Le <annxle9844@gmail.com>
2024-10-10 23:24:20 -0500

    reformat portfolio

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+
+//   function toggleDescription(element) {
+//     // Toggle the "open" class on the description elements
+//     const experienceSection = element.parentElement;
+//     const descriptions = experienceSection.querySelectorAll('.description');
+//     descriptions.forEach(description => {
+//       description.classList.toggle('open');
+//     });
+
+//     // Toggle the rotation of the ">" symbol
+//     element.classList.toggle('open');
+//   }
+
